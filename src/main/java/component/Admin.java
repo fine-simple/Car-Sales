d506930144b793f340fa184875ecffd7058f1c3d
omarkhaled646 <omarkhaled225577@gmail.com>
2020-12-24 15:55:25 +0200

    main updated

--- src/main/java/component/Admin.java
+++ src/main/java/component/Admin.java
@@ -1,7 +1,12 @@
 package main.java.component;
 
-public class Admin extends User{
-    Admin(String FN, String E, String PW) {
-        super(FN, E, PW);
-    }
+public class Admin extends User {
+	Admin(String FN, String E, String PW) {
+		super(FN, E, PW);
+	}
+
+	public void add(Car car) {
+
+	}
+
 }
