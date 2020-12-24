22f958120972e0a8983bc6d9c855d458c30da4cc
omarkhaled646 <omarkhaled225577@gmail.com>
2020-12-24 12:54:45 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,28 +5,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import main.java.component.Vehicle;
 
 public class Main extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// Array of Vehicles
+		Vehicle[] vehicles = new Vehicle[1000];
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        //Array of Vehicles
-        Vehicle []vehicles = new Vehicle[1000];
+		// Start Login Page
+		Parent root = FXMLLoader.load(getClass().getResource("../gui/fxml/login.fxml"));
+		Scene scene = new Scene(root);
 
-        //Start Login Page
-        Parent root = FXMLLoader.load(getClass().getResource("../gui/fxml/login.fxml"));
-        Scene scene = new Scene(root);
-        
-        //Stage Settings
-        primaryStage.setResizable(false);
-        primaryStage.setTitle("Login");
+		// Stage Settings
+		primaryStage.setResizable(false);
+		primaryStage.setTitle("Login");
 
-        //Display window
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+		// Display window
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 }
