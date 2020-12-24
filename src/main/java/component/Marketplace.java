346782d1333405f691923c6bb80c914afb2ed08c
omarkhaled646 <omarkhaled225577@gmail.com>
2020-12-24 14:49:25 +0200

    Update Marketplace.java

--- src/main/java/component/Marketplace.java
+++ src/main/java/component/Marketplace.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 
 public class Marketplace {
 
-    public Marketplace() {
-        
-    }
+	// Array list
+	public static ArrayList<Car> arrayOfCars = new ArrayList<>();
+
+	public void add(Car c) {
+		arrayOfCars.add(c);
+	}
+
 }
