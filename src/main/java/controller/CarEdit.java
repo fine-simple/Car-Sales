fe69a817032466481abadc72a59023942356ac35
omarkhaled646 <omarkhaled225577@gmail.com>
2020-12-29 20:12:13 +0200

    Update CarEdit.java

--- src/main/java/controller/CarEdit.java
+++ src/main/java/controller/CarEdit.java
@@ -1,10 +1,13 @@
 package main.java.controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,7 +15,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import main.java.component.Car;
 
-public class CarEdit {
+public class CarEdit implements Initializable {
 
 	private static Car activeCar = null;
 	@FXML
@@ -44,6 +47,13 @@ public class CarEdit {
 
 	public static void setActiveCar(Car activeCar) {
 		CarEdit.activeCar = activeCar;
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		company.setText(activeCar.getName());
+		model.setText(activeCar.getModel());
+		year.setText(activeCar.getDate() + "");
 	}
 
 }
